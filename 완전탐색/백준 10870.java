3d5bb93e4776c6699d0b7090a8f911182e0367f7
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2022-01-09 20:07:33 +0900

    피보나치수열

---
+++ 완전탐색/백준 10870.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+
+	public static Scanner scanner=new Scanner(System.in);
+	public static int pivo(int n) {
+	
+		int answer=0;
+		
+		if(n==0)return 0;
+		if(n==1)return 1;
+		
+		answer=pivo(n-1)+pivo(n-2);
+		
+		
+		
+		return answer;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n= scanner.nextInt();
+		System.out.print(pivo(n));
+
+		}
+}
