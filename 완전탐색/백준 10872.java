7c8928c8adb66f659762f701c94de472d23501b5
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2022-01-09 20:04:50 +0900

    factorial 구현

---
+++ 완전탐색/백준 10872.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static Scanner scanner=new Scanner(System.in);
+	public static int factorial(int n) {
+		int answer=0;
+			if(n<=1) {
+				return 1;
+			}
+			answer=n*factorial(n-1);
+	
+		return answer;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n= scanner.nextInt();
+		System.out.print(factorial(n));
+
+		}
+}
