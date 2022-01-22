d6433a9155286a47014619791208a3cb4ac571ab
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2022-01-22 20:47:04 +0900

    수 이어쓰기 1

---
+++ 완전탐색/백준 1748.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	public static Scanner scan =new Scanner(System.in);
+	public static StringBuilder sb=new StringBuilder();
+	static String t;
+	static int k=0;
+	static int cnt=0,tmp2=0;
+	static int flag;
+	public static void main(String[] args) {
+			t=scan.next();
+			k=t.length();	//몇자리 수인지
+			for(int a=0;a<k-1;a++) {		//0: 1의자리수 n-1: n의 자리수
+				cnt+=(a+1)*9*Math.pow(10, a);
+			}
+			cnt+=k*(Integer.parseInt(t)-(Math.pow(10, k-1)-1));
+			System.out.println(cnt);
+		}
+
+	
+}
