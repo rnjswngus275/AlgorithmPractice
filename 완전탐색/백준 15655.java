6732c83c5a49ce6980432d8df709142a0dd56843
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2022-01-24 16:45:55 +0900

    n과 m(6)

---
+++ 완전탐색/백준 15655.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+	public static Scanner scan =new Scanner(System.in);
+	public static StringBuilder sb=new StringBuilder();
+	static int n,m;
+	static int cnt=0,tmp2=0;
+	static int arr[];
+	static int tmp[];
+	static int flag;
+	public static void main(String[] args) {
+			n=scan.nextInt();
+			m=scan.nextInt();
+			arr=new int[n];
+			tmp=new int[n];
+
+			for(int a=0;a<n;a++) {	
+				arr[a]=scan.nextInt();
+				}
+			Arrays.sort(arr);
+			solve(0,0,false);
+			System.out.print(sb);
+		}
+
+	public static void solve(int pos,int choice,boolean flag) {	//n개의 자연수 중 m개를 고른 수열
+		if(pos>n) {
+			return;
+		}
+		
+		if(flag==true) {
+			tmp[pos-1]=1;
+		}
+		if(flag==false&&pos!=0) {
+			tmp[pos-1]=0;
+		}
+		if(choice==m) {
+			for(int i=0;i<n;i++) {
+				if(tmp[i]==1) {
+					sb.append(arr[i]+" ");
+				}
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		
+		solve(pos+1,choice+1,true);
+		solve(pos+1,choice,false);
+		return;
+	}
+}
