4e6232ed3024b0e527ba894f1dd21d11084e751a
rnjswngus275 <38181228+rnjswngus275@users.noreply.github.com>
2022-01-24 16:13:08 +0900

    n과m(5)

---
+++ 완전탐색/백준 15654.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+	public static Scanner scan =new Scanner(System.in);
+	public static StringBuilder sb=new StringBuilder();
+	static int n,m;
+	static int cnt=0,tmp2=0;
+	static int arr[];
+	static int tmp[];
+	static int flag;
+	public static void main(String[] args) {
+			n=scan.nextInt();
+			m=scan.nextInt();
+			arr=new int[n];
+			tmp=new int[m];
+			for(int a=0;a<n;a++) {	
+				arr[a]=scan.nextInt();
+				}
+			Arrays.sort(arr);
+			solve(0,0);
+			System.out.print(sb);
+		}
+
+	public static void solve(int cnt,int used) {	//n개의 자연수 중 m개를 고른 수열
+
+		if(cnt==m) {
+			for(int i=0;i<m;i++) {
+				sb.append(tmp[i]+" ");				
+				
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<n;i++) {
+			if((used&1<<i)!=0) {//i번째 원소가 있으면 
+				continue;
+			}
+			tmp[cnt]=arr[i];
+			solve(cnt+1,used|1<<i);
+		}
+		
+	}
+}
