06db069e57d56fa761f83f6e4edc4d2a37cf3b72
JUHYEON KWON <38181228+rnjswngus275@users.noreply.github.com>
2022-05-02 14:52:27 +0900

    이전 순열

---
+++ 수학 관련 알고리즘/백준 10972.java
@@ -0,0 +1,53 @@
+
+import java.util.*;
+public class Main {
+
+	public static Scanner scan =new Scanner(System.in);
+	public static StringBuilder sb=new StringBuilder();
+	public static int[] arr,visited,arr3;
+	static int[] perm;
+	static int N,num=0,cnt2;
+	static boolean flag;
+    public static void main(String[] args) {
+    	//입력
+    	N=scan.nextInt();
+    	arr=new int[N];
+    	perm=new int[N];
+    	visited=new int[N+1];
+    	for(int i=0;i<N;i++) {
+    		arr[i]=scan.nextInt();
+    	}
+    	if(nextperm(arr))
+    	{
+    		for(int i=0;i<N;i++) {
+        		System.out.print(arr[i]+" ");
+        	}
+    	}
+    	else {
+    		System.out.print(-1);
+    	}
+    	
+    }
+    static boolean nextperm(int[] arr) {
+    	int i=arr.length-1;
+    	int j=arr.length-1;
+    	while(i>0&&arr[i-1]>=arr[i]) i--;
+    	if(i<=0) return false;
+
+    	while(arr[j]<=arr[i-1]) j--;
+    	
+    	swap(arr,i-1,j);
+    	j=arr.length-1;
+    	while(i<j) {
+    		swap(arr,i,j);
+    		i++;
+    		j--;
+    	}
+    	return true;
+    }
+    static void swap(int[] arr, int idx1, int idx2) {
+    	int temp=arr[idx1];
+    	arr[idx1]=arr[idx2];
+    	arr[idx2]=temp;
+    }
+ }
