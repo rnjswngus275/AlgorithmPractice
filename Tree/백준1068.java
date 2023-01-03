4d64bdd6312ed48b1d806cb734bdab4460cdc8e6
rnjswngus <rnjswngus275@gmail.com>
2023-01-03 23:05:55 +0900

    tree

--- Tree/백준1068.java
+++ Tree/백준1068.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class 백준1068 {
 	static Scanner scan=new Scanner(System.in);
 	static ArrayList<ArrayList<Integer>> list=new ArrayList<>();
-	static int n,erase;
+	static int n,erase,cnt;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	    n=scan.nextInt();
@@ -16,43 +16,37 @@ public class 백준1068 {
 	        int parent=scan.nextInt();
 	        if(parent!=-1) {
 	            list.get(parent).add(i);
-	            list.get(i).add(parent);
-	        }else root=parent;
+	        }else root=i;
 	    }
 	    erase=scan.nextInt();
 	    
 	    if(erase==root) {
+	        System.out.println(0);
+	    }else {
 	        
-	    }
-	    for(int i=list.get(erase).size()-1;i>=0;i--) {
-	        if(list.get(erase).get(i)<erase) {
-	            list.get(erase).remove(i);
+	        for(int i=0;i<n;i++) {
+	            if(list.get(i).contains(erase)) {
+	                list.get(i).remove(list.get(i).indexOf(erase));
+	            }
 	        }
+	        
+	        sol(root);
+	        
+    
+	        System.out.println(cnt); 
 	    }
-	    sol(erase);
-	    
-	    int cnt=0;
-	    for(int i=0;i<list.size();i++) {
-	        if(list.get(i).size()==1) {//부모가 1개이면 리프노드
-	            cnt++;
-	        }
-	    }
-	    
-	    System.out.println(cnt); 
+	   
 	}
 	
-	public static void sol(int del) {//dfs돌면서 리프노드 지우기
-	   int size= list.get(del).size();
-	   
-	   if(size==1) {//리프노드이면 지운다
-	       list.get(del).set(0,-1);
+	public static void sol(int idx) {
+	   if(list.get(idx).isEmpty()) {
+	       cnt++;
 	       return;
 	   }
-	   for(int i=size-1;i>-1;i--) {
-	       int node=list.get(del).get(i);
-	       if(node>del) {//삭제하는 노드의 자식 노드일때 삭제
-	           sol(node);
-               list.get(del).set(i,-1);
+	   else {
+	       for(int a:list.get(idx)) {
+	           sol(a);
+	           
 	       }
 	   }
 	}
